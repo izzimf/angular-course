247aeb8209cc4dd258ac21560fef62774c10ca21
Islambek <Jlugep17@gmail.com>
2025-09-26 00:31:54 +0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // <-- добавили
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], // <-- добавили CommonModule
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angularcoursekbtu';
+
+  trackByTitle(index: number, item: { title: string; link: string }): string {
+    return item.title;
+  }
 }
