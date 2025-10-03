b5016149fa6c388771dc6ad9a0febb79c4455923
Zhakyayev Islambek <124435153+izzimf@users.noreply.github.com>
2025-10-03 13:58:29 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // 1. Interpolation
+  
   title = 'About Our Team';
   mission = 'We build delightful software for our users.';
 
-  // 2. Property Binding
+  
   photoUrl = 'https://upload.wikimedia.org/wikipedia/ru/thumb/c/cd/Football_Federation_of_Kazakhstan_Logo.svg/1920px-Football_Federation_of_Kazakhstan_Logo.svg.png';
   isButtonDisabled = false;
 
-  // 3. Event Binding
+  
   likes = 0;
   showMessage = false;
 
-  // 4. Two-Way Binding
+ 
   name = '';
   email = '';
   subscribed = false;
 
-  // Methods
+ 
   addLike() {
     this.likes++;
   }
